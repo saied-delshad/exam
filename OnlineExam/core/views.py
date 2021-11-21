578018d6b3013eb9737419c0f28c7063f4af4c8d
Alireza <alireza.hedayat@gmail.com>
2021-11-21 19:09:25 +0330

    core view

--- OnlineExam/core/views.py
+++ OnlineExam/core/views.py
@@ -16,7 +16,7 @@ class IndexTemplateView(LoginRequiredMixin, TemplateView):
 
 
     def get_template_names(self):
-        if not settings.DEBUG:
+        if settings.DEBUG:
             template_name = 'index-dev.html'
         else:
             template_name = 'index.html'
