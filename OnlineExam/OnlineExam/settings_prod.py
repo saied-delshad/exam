79e9c32e1ec5dbc8aef94bd746d7474a313aee92
Alireza <alireza.hedayat@gmail.com>
2023-03-27 09:16:09 +0330

    settings changed for static

--- OnlineExam/OnlineExam/settings_prod.py
+++ OnlineExam/OnlineExam/settings_prod.py
@@ -15,3 +15,20 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR , 'static'),
+    os.path.join(BASE_DIR , 'frontend\dist')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- OnlineExam/OnlineExam/settings_shared.py
+++ OnlineExam/OnlineExam/settings_shared.py
@@ -162,20 +162,6 @@ LOGIN_URL = "accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR , 'static'),
-    os.path.join(BASE_DIR , 'frontend\dist')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 # Custom User Model
 AUTH_USER_MODEL = 'users.CustomUser'
