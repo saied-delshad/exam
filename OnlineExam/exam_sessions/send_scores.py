a95a7b4ec552678b3006011131faa4a3436f8c5b
alireza <alireza.hedayat@gmail.com>
2023-07-09 16:49:43 -0700

    TLSv1_3

--- OnlineExam/exam_sessions/send_scores.py
+++ OnlineExam/exam_sessions/send_scores.py
@@ -1,7 +1,21 @@
 import requests
-import json
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
+import ssl
 
-URL = 'https://bpms.cao.ir/NetForm/Service/examresult/request'
+
+class TlsAdapter(HTTPAdapter):
+
+  def __init__(self, ssl_options=0, **kwargs):
+    self.ssl_options = ssl_options
+    super(TlsAdapter, self).__init__(**kwargs)
+
+  def init_poolmanager(self, *pool_args, **pool_kwargs):
+    ctx = ssl_.create_urllib3_context(cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+    self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
+
+URL = 'https://bpms.cao.ir/NetForm/Service/irexamresult/request'
 
 
 def send_score(ref_code, nid, score, date, passed=0):
@@ -13,6 +27,9 @@ def send_score(ref_code, nid, score, date, passed=0):
             "date": date,
             "passed": passed
           }
-    response = requests.post(URL, json=js, verify=False)
+    sess = requests.session()
+    adp = TlsAdapter(ssl.OP_NO_TLSv1_3 | ssl.OP_NO_TLSv1_3)
+    sess.mount(URL, adp)
+    response=sess.post(URL, json=js)
 
     return response.json()
