6267482dcb7eff85efad2c0d6dc4da2466d42ae7
root <root@aviation-test.com>
2024-05-18 08:58:27 +0430

    update

--- OnlineExam/OnlineExam/settings_dev.py
+++ OnlineExam/OnlineExam/settings_dev.py
@@ -5,7 +5,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*', 'meraj.exama.ir']
